5ba22a06c585b3d924568f64c9668813e10c888e
aoffahad-admin <omar.faruque@nexdecade.com>
2024-11-28 10:42:12 +0600

    debugShowCheckedModeBanner false

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_login_auth/presentation/screen/login_screen.dart';
 import 'package:firebase_login_auth/presentation/screen/widget_tree.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   // await Firebase.initializeApp(
@@ -20,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: WidgetTree(),
     );
   }
